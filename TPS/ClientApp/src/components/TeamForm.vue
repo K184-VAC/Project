cf0f7792527db7bb632de9c1d51ab5d526e6c48a
Lukasamba <61199227+Lukasamba@users.noreply.github.com>
2022-04-01 16:51:41 +0300

    Pridėta komandos sukurimo forma HTML

---
+++ TPS/ClientApp/src/components/TeamForm.vue
@@ -0,0 +1,25 @@
+﻿<script setup lang="ts">
+
+</script>
+<template>
+    <form class="mt-4 mb-4 ms-4">
+        <div class="mb-3">
+            <label for="name" class="form-label">Komandos pavadinimas</label>
+            <input type="text" class="form-control w-25" id="name">
+        </div>
+        <div class="mb-3">
+            <label for="members" class="form-label">Komandos nariai</label>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    Nariai
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li><a class="dropdown-item">Vardenis Pavardenis</a></li>
+                    <li><a class="dropdown-item">Antanas Antanaitis</a></li>
+                    <li><a class="dropdown-item">Petras Petraitis</a></li>
+                </ul>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Sukurti</button>
+    </form>
+</template>
--- TPS/ClientApp/src/components/Teams.vue
+++ TPS/ClientApp/src/components/Teams.vue
@@ -17,37 +17,37 @@
 </script>
 <template>
     <div class="mt-4 mb-4 ms-4">
-        <button type="button" class="ml-3 btn btn-primary">Sukurti komandą</button>
+        <router-link to="/teams/add" class="ml-3 btn btn-primary" tag="button">Sukurti komandą</router-link>
     </div>
     <div class="row">
         <div class="col-2 mt-4 mb-4 ms-4">
             <div class="list-group" id="list-tab" role="tablist">
-                <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Team 1</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Team 2</a>
-                <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Team 3</a>
-                <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Team 4</a>
+                <a class="list-group-item list-group-item-action active" id="list-team1-list" data-bs-toggle="list" href="#list-team1" role="tab" aria-controls="list-team1">Komanda 1</a>
+                <a class="list-group-item list-group-item-action" id="list-team2-list" data-bs-toggle="list" href="#list-team2" role="tab" aria-controls="list-team2">Komanda 2</a>
+                <a class="list-group-item list-group-item-action" id="list-team3-list" data-bs-toggle="list" href="#list-team3" role="tab" aria-controls="list-team3">Komanda 3</a>
+                <a class="list-group-item list-group-item-action" id="list-team4-list" data-bs-toggle="list" href="#list-team4" role="tab" aria-controls="list-team4">Komanda 4</a>
             </div>
         </div>
         <div class="col-8 mt-4 mb-4 ms-4">
             <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+                <div class="tab-pane fade show active" id="list-team1" role="tabpanel" aria-labelledby="list-team1-list">
                     {{team1.planning}}<br>
                     {{team1.standup}}<br>
                     {{team1.retro}}<br>
                 </div>
-                <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+                <div class="tab-pane fade" id="list-team2" role="tabpanel" aria-labelledby="list-team2-list">
                     {{team2.planning}}<br>
                     {{team2.standup}}<br>
                     {{team2.retro}}<br>
 
                 </div>
-                <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">
+                <div class="tab-pane fade" id="list-team3" role="tabpanel" aria-labelledby="list-team3-list">
                     {{team3.planning}}<br>
                     {{team3.standup}}<br>
                     {{team3.retro}}<br>
 
                 </div>
-                <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+                <div class="tab-pane fade" id="list-team4" role="tabpanel" aria-labelledby="list-team4-list">
                     {{team4.planning}}<br>
                     {{team4.standup}}<br>
                     {{team4.retro}}<br>
