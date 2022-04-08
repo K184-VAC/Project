f22490a7161aa7756ca31b171eef94fa403efdca
Karolis2011 <Karolis2011@users.noreply.github.com>
2022-04-08 23:31:31 +0300

    Login controller finish

--- TPS/Controllers/LoginController.cs
+++ TPS/Controllers/LoginController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TPS.Data;
+using TPS.Data.Model;
+using TPS.Extensions;
 
 namespace TPS.Controllers;
 
@@ -9,11 +13,24 @@ namespace TPS.Controllers;
 [ApiController]
 public class LoginController : ControllerBase
 {
+    private readonly TPSDataContext dataContext;
 
+    public LoginController(TPSDataContext dataContext)
+    {
+        this.dataContext = dataContext;
+    }
 
     [HttpPost]
-    public IActionResult LoginRegister()
+    public async Task<IActionResult> LoginRegister()
     {
+        var userId = User.GetObjectId();
+        var user = await dataContext.Users.FirstAsync((u) => u.Id == userId);
+        if (user == null)
+        {
+            user = new User(userId, User.GetEmail(), User.GetName());
+            await dataContext.Users.AddAsync(user);
+            await dataContext.SaveChangesAsync();
+        }
         return Ok();
     }
 }
